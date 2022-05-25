f1892ec2b52cd2327965f6672337c582fa8fe11b
moni-que <monicahjustus@gmail.com>
2022-05-25 15:35:09 +0300

    create app

---
+++ news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+from . import views
+
+urlpatterns=[
+    re_path(r'^$',views.news_today,name='newsToday'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    re_path(r'^search/', views.search_results, name='search_results')
+]
